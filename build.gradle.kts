558821e30be74d0e77c4a530a3e550a3349c8069
JRivera340 <jrivera@example.com>
2026-05-14 23:17:05 -0500

    fix: restore missing plugins block in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+plugins {
     id("org.springframework.boot") version "3.2.4" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
     id("com.google.cloud.tools.jib") version "3.4.1" apply false
