b6f40dd45576c3cce59b52936cf5b4b15ec8b480
JRivera340 <Riveragonzalezjoshua404@gmail.com>
2026-06-09 19:51:19 -0500

    fix: agregar spring-boot-starter-actuator para health probes y metricas prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ subprojects {
     dependencies {
         "implementation"(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
         "testImplementation"(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
+        // Actuator: health groups (liveness/readiness) y endpoint /actuator/prometheus
+        "implementation"("org.springframework.boot:spring-boot-starter-actuator")
         "compileOnly"("org.projectlombok:lombok")
         "annotationProcessor"("org.projectlombok:lombok")
         "testCompileOnly"("org.projectlombok:lombok")
